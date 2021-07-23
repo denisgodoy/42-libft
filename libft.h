9dc28604b4c230de461ca7d583084019fd7c4824
denisgodoy <denisgodoy1@gmail.com>
2021-07-23 20:39:40 -0300

    update ft_strlen function type

--- libft.h
+++ libft.h
@@ -1,15 +1,15 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int		ft_atoi(const char	*nptr);
-int		ft_isalnum(int	c);
-int		ft_isalpha(int	c);
-int		ft_isascii(int	c);
-int		ft_isdigit(int	c);
-int		ft_isprint(int	c);
-char	*ft_strchr(const char	*s, int	c);
-int		ft_strlen(const char	*s);
-int		ft_tolower(int	c);
-int		ft_toupper(int	c);
+int				ft_atoi(const char	*nptr);
+int				ft_isalnum(int	c);
+int				ft_isalpha(int	c);
+int				ft_isascii(int	c);
+int				ft_isdigit(int	c);
+int				ft_isprint(int	c);
+char			*ft_strchr(const char	*s, int	c);
+unsigned int	ft_strlen(const char	*s);
+int				ft_tolower(int	c);
+int				ft_toupper(int	c);
 
 #endif
