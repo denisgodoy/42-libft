913088a4b11740539164d7fe0e6e7db3805be474
denisgodoy <denisgodoy1@gmail.com>
2021-07-24 12:42:44 -0300

    norminette OK

--- main/ft_tolower.c
+++ main/ft_tolower.c
@@ -11,7 +11,10 @@ int	main(void)
 	al = 'A';
 	al2 = 112;
 	not_al = 52;
-	printf("Test: %c\nExpected: %c, Got: %c\n\n", al, tolower(al), ft_tolower(al));
-	printf("Test: %c\nExpected: %c, Got: %c\n\n", al2, tolower(al2), ft_tolower(al2));
-	printf("Test: %c\nExpected: %c, Got: %c\n", not_al, tolower(not_al), ft_tolower(not_al));
+	printf("Test: %c\nExpected: %c, Got: %c\n\n",
+		al, tolower(al), ft_tolower(al));
+	printf("Test: %c\nExpected: %c, Got: %c\n\n",
+		al2, tolower(al2), ft_tolower(al2));
+	printf("Test: %c\nExpected: %c, Got: %c\n",
+		not_al, tolower(not_al), ft_tolower(not_al));
 }
--- main/ft_toupper.c
+++ main/ft_toupper.c
@@ -11,7 +11,10 @@ int	main(void)
 	al = 'a';
 	al2 = 90;
 	not_al = 52;
-	printf("Test: %c\nExpected: %c, Got: %c\n\n", al, toupper(al), ft_toupper(al));
-	printf("Test: %c\nExpected: %c, Got: %c\n\n", al2, toupper(al2), ft_toupper(al2));
-	printf("Test: %c\nExpected: %c, Got: %c\n", not_al, toupper(not_al), ft_toupper(not_al));
+	printf("Test: %c\nExpected: %c, Got: %c\n\n",
+		al, toupper(al), ft_toupper(al));
+	printf("Test: %c\nExpected: %c, Got: %c\n\n",
+		al2, toupper(al2), ft_toupper(al2));
+	printf("Test: %c\nExpected: %c, Got: %c\n",
+		not_al, toupper(not_al), ft_toupper(not_al));
 }
