b78175f14288b211f5614583cde0d5d1f4fac4dc
denisgodoy <denisgodoy1@gmail.com>
2021-08-05 10:57:47 -0300

    update libft

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 10:40:51 by degabrie          #+#    #+#             */
-/*   Updated: 2021/08/04 19:50:50 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/08/05 10:45:46 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,22 @@ char	*ft_strtrim(char const	*s1, char const	*set)
 {
 	size_t		i;
 	size_t		j;
+	size_t		k;
 	char		*ptr;
 
 	i = 0;
 	j = i;
-	ptr = (char *)malloc(ft_strlen(s1));
+	ptr = (char *)malloc(ft_strlen(s1) + 1);
 	while (s1[i] != '\0')
 	{
-		if (s1[i] != *set)
+		k = 0;
+		while (set[k] != '\0')
+		{
+			if (s1[i] == set[k])
+				break ;
+			k++;
+		}
+		if (s1[i] != set[k])
 			ptr[j++] = s1[i];
 		i++;
 	}
